6ba17f11571d5fa433d04f62cfdd9d72b123fb02
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-03-22 00:55:27 +0800

    Add tests for JsonSerializableAddressBook

--- src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableAddressBookTest.java
@@ -2,15 +2,18 @@ package seedu.address.storage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.JsonUtil;
 import seedu.address.model.AddressBook;
 import seedu.address.testutil.TypicalPersons;
+
 
 public class JsonSerializableAddressBookTest {
 
@@ -20,6 +23,12 @@ public class JsonSerializableAddressBookTest {
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAddressBook.json");
     private static final Path IMPLICIT_CLASS_SPACE_FILE =
             TEST_DATA_FOLDER.resolve("personWithImplicitClassSpaceAddressBook.json");
+    private static final Path MISSING_NAME_PERSON_FILE =
+            TEST_DATA_FOLDER.resolve("missingNamePersonAddressBook.json");
+    private static final Path JSON_NULL_NAME_PERSON_FILE =
+            TEST_DATA_FOLDER.resolve("jsonNullNamePersonAddressBook.json");
+    private static final Path DUPLICATE_CLASS_SPACE_FILE =
+            TEST_DATA_FOLDER.resolve("duplicateClassSpaceAddressBook.json");
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
@@ -107,5 +116,52 @@ public class JsonSerializableAddressBookTest {
                         new seedu.address.model.classspace.ClassSpaceName("Implicit-Class-Space"));
 
         assertTrue(addressBookFromFile.hasClassSpace(expectedClassSpace));
+    }
+
+    @Test
+    public void constructor_readOnlyAddressBook_convertsCorrectly() {
+        AddressBook typicalAddressBook = TypicalPersons.getTypicalAddressBook();
+        JsonSerializableAddressBook serializable = new JsonSerializableAddressBook(typicalAddressBook);
+
+        // Should not throw exceptions, just to check equality.
+        try {
+            assertEquals(typicalAddressBook, serializable.toModelType());
+        } catch (Exception e) {
+            throw new AssertionError("Conversion should not fail.", e);
+        }
+    }
+
+    @Test
+    public void toModelType_missingName_generatesCorrectWarning() throws Exception {
+        assertMissingNameWarningIsGenerated(MISSING_NAME_PERSON_FILE);
+    }
+
+    @Test
+    public void toModelType_jsonNullName_generatesCorrectWarning() throws Exception {
+        assertMissingNameWarningIsGenerated(JSON_NULL_NAME_PERSON_FILE);
+    }
+
+    @Test
+    public void toModelType_duplicateClassSpaces_throwsIllegalValueException() throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_CLASS_SPACE_FILE,
+                JsonSerializableAddressBook.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_CLASS_SPACE,
+                dataFromFile::toModelType);
+    }
+
+    /**
+     * Helper method to read JSON file and assert that it produces "missing name" warning.
+     *
+     * @param filePath Path of JSON file.
+     * @throws Exception
+     */
+    private void assertMissingNameWarningIsGenerated(Path filePath) throws Exception {
+        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(filePath,
+                JsonSerializableAddressBook.class).get();
+        dataFromFile.toModelType(); // This populates the warnings
+
+        assertEquals(1, dataFromFile.getLoadWarnings().size());
+        assertTrue(dataFromFile.getLoadWarnings().get(0).contains("entry #1 (missing name)"));
+        assertEquals(0, dataFromFile.toModelType().getPersonList().size());
     }
 }
